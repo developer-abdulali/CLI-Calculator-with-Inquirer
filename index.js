a3288b5f8ee631f774c46d36fa8d3c193cb926ed
developer-abdulali <ab.ali.soomro@gmail.com>
2023-08-18 17:33:51 +0500

    maked calculator

---
+++ index.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "enter 1st number",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "enter 2nd number",
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select the operator",
+        choices: ["Add", "Substract", "Multiply", "Division", "Percentage"]
+    }
+]);
+let result;
+if (answer.operator === "Add") {
+    result = answer.num1 + answer.num2;
+}
+else if (answer.operator === "Substract") {
+    result = answer.num1 - answer.num2;
+}
+else if (answer.operator === "Multiply") {
+    result = answer.num1 * answer.num2;
+}
+else if (answer.operator === "Division") {
+    result = answer.num1 / answer.num2;
+}
+else if (answer.operator === "Percentage") {
+    result = answer.num1 % answer.num2;
+}
+console.log(answer);
+console.log(`Result: ${result}`);
---
+++ index.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+  {
+    name: "num1",
+    type: "number",
+    message: "enter first number",
+  },
+  {
+    name: "num2",
+    type: "number",
+    message: "enter second number",
+  },
+  {
+    name: "operator",
+    type: "list",
+    message: "Select the operator",
+    choices: ["Add", "Substract", "Multiply", "Division", "Percentage"],
+  },
+]);
+
+let result;
+
+if (answer.operator === "Add") {
+  result = answer.num1 + answer.num2;
+} else if (answer.operator === "Substract") {
+  result = answer.num1 - answer.num2;
+} else if (answer.operator === "Multiply") {
+  result = answer.num1 * answer.num2;
+} else if (answer.operator === "Division") {
+  result = answer.num1 / answer.num2;
+}
+
+console.log(answer);
+console.log(`Result: ${result}`);
